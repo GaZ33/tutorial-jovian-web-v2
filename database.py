15fdafc9632bd2ab06d662aeeeaccf646827e05d
GaZ33 <gasinho.ga@gmail.com>
2023-12-05 16:27:49 -0300

    teste

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
-
-db_connection = os.environ['DB_CONNECTION']
-engine = create_engine(db_connection)
+secreto = os.environ.get('DB_CONNECTION_MYSQL')
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+print(secreto)
+#db_connection = os.environ.get['DB_CONNECTION_MYSQL']
+#engine = create_engine(secreto)
 
 #"mysql+pymysql://root:gabriel2012@127.0.0.1/joviancareers?charset=utf8mb4"
