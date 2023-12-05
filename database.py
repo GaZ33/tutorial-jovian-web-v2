6472791bc96bc690e8daf3b6d48826baf924552f
GaZ33 <gasinho.ga@gmail.com>
2023-12-05 16:14:55 -0300

    updating

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
+import os
 
-engine = create_engine("mysql+pymysql://root:gabriel2012@127.0.0.1/joviancareers?charset=utf8mb4")
+db_connection = os.environ['DB_CONNECTION']
+engine = create_engine(db_connection)
 
+#"mysql+pymysql://root:gabriel2012@127.0.0.1/joviancareers?charset=utf8mb4"
