d5e0242302e0920121f5ff992445bd36341dce43
GaZ33 <gasinho.ga@gmail.com>
2023-12-05 16:36:11 -0300

    Alterando algumas ocisas

--- database.py
+++ database.py
@@ -1,11 +1,5 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
-secreto = os.environ.get('DB_CONNECTION_MYSQL')
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
-print(secreto)
-#db_connection = os.environ.get['DB_CONNECTION_MYSQL']
-#engine = create_engine(secreto)
 
-#"mysql+pymysql://root:gabriel2012@127.0.0.1/joviancareers?charset=utf8mb4"
+engine = create_engine("mysql+pymysql://root:teste123@127.0.0.1/joviancareers?charset=utf8mb4")
